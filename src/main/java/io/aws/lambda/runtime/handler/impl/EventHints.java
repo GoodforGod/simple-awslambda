238335e1fc65de71582a336795c04d9217b6870e
Anton Kurako <goodforgod.dev@gmail.com>
2021-06-14 11:56:24 +0300

    [2.0.0-SNAPSHOT] EventHints hint from fields -> all public

--- src/main/java/io/aws/lambda/runtime/handler/impl/EventHints.java
+++ src/main/java/io/aws/lambda/runtime/handler/impl/EventHints.java
@@ -6,7 +6,7 @@ import io.aws.lambda.events.gateway.*;
 import io.micronaut.core.annotation.TypeHint;
 
 @TypeHint(
-        accessType = { TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS, TypeHint.AccessType.ALL_DECLARED_FIELDS },
+        accessType = { TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS, TypeHint.AccessType.ALL_PUBLIC },
         value = {
                 APIGatewayProxyEvent.class,
                 APIGatewayProxyEvent.RequestIdentity.class,
