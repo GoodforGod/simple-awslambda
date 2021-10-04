3ecda7de54b1a6945f4b0731b226ba9d8acf4155
Anton Kurako <goodforgod.dev@gmail.com>
2021-10-05 01:19:30 +0300

    [0.10.0-SNAPSHOT] GraalVMHints annotation processor and annotations added GraalVMHints with hints info added

---
+++ src/main/java/io/goodforgod/aws/lambda/simple/hint/GraalVMHints.java
@@ -0,0 +1,43 @@
+package io.goodforgod.aws.lambda.simple.hint;
+
+import io.aws.lambda.events.BodyBase64Event;
+import io.aws.lambda.events.BodyEvent;
+import io.aws.lambda.events.gateway.*;
+import io.goodforgod.graalvm.hint.annotation.InitializationHint;
+import io.goodforgod.graalvm.hint.annotation.NativeImageHint;
+import io.goodforgod.graalvm.hint.annotation.TypeHint;
+
+/**
+ * @author Anton Kurako (GoodforGod)
+ * @since 27.09.2021
+ */
+@TypeHint(
+        value = { TypeHint.AccessType.ALL_DECLARED },
+        types = {
+                APIGatewayProxyEvent.class,
+                APIGatewayProxyEvent.RequestIdentity.class,
+                APIGatewayProxyEvent.ProxyRequestContext.class,
+                APIGatewayProxyResponse.class,
+                APIGatewayV2HTTPEvent.class,
+                APIGatewayV2HTTPEvent.RequestContext.class,
+                APIGatewayV2HTTPEvent.RequestContext.Http.class,
+                APIGatewayV2HTTPEvent.RequestContext.CognitoIdentity.class,
+                APIGatewayV2HTTPEvent.RequestContext.Authorizer.class,
+                APIGatewayV2HTTPEvent.RequestContext.Authorizer.JWT.class,
+                APIGatewayV2HTTPResponse.class,
+                APIGatewayV2WebSocketEvent.class,
+                APIGatewayV2WebSocketEvent.RequestContext.class,
+                APIGatewayV2WebSocketEvent.RequestIdentity.class,
+                APIGatewayV2WebSocketResponse.class,
+
+                BodyEvent.class,
+                BodyBase64Event.class
+        })
+@InitializationHint(typeNames = {
+        "io.goodforgod.aws.lambda.simple",
+        "org.slf4j.impl",
+        "org.slf4j.LoggerFactory"
+})
+@NativeImageHint(options = { "-H:+PrintClassInitialization", "-H:+InlineBeforeAnalysis" })
+interface GraalVMHints {
+}
