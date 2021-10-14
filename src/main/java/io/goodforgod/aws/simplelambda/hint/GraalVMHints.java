bd60af6e317f4921c42f57d5d480c166c056f690
Anton Kurako <goodforgod.dev@gmail.com>
2021-10-15 02:03:34 +0300

    [0.12.0-SNAPSHOT] GraalVMHints reduced

--- src/main/java/io/goodforgod/aws/simplelambda/hint/GraalVMHints.java
+++ src/main/java/io/goodforgod/aws/simplelambda/hint/GraalVMHints.java
@@ -1,14 +1,10 @@
 package io.goodforgod.aws.simplelambda.hint;
 
 import io.goodforgod.aws.lambda.events.*;
-import io.goodforgod.aws.lambda.events.cognito.CognitoEvent;
-import io.goodforgod.aws.lambda.events.cognito.CognitoUserPoolEvent;
 import io.goodforgod.aws.lambda.events.dynamodb.DynamoDBEvent;
 import io.goodforgod.aws.lambda.events.gateway.*;
 import io.goodforgod.aws.lambda.events.kinesis.KinesisEvent;
 import io.goodforgod.aws.lambda.events.s3.S3BatchEvent;
-import io.goodforgod.aws.lambda.events.s3.S3EventNotification;
-import io.goodforgod.aws.lambda.events.s3.S3ObjectLambdaEvent;
 import io.goodforgod.graalvm.hint.annotation.InitializationHint;
 import io.goodforgod.graalvm.hint.annotation.ResourceHint;
 import io.goodforgod.graalvm.hint.annotation.TypeHint;
@@ -40,22 +36,11 @@ import io.goodforgod.graalvm.hint.annotation.TypeHint;
                 BodyEvent.class,
                 Base64BodyEvent.class,
 
-                ActiveMQEvent.class,
-                ActiveMQEvent.ActiveMQMessage.class,
-                ActiveMQEvent.Destination.class,
-                ConnectEvent.class,
-                ConnectEvent.ContactData.class,
-                ConnectEvent.CustomerEndpoint.class,
-                ConnectEvent.Details.class,
-                ConnectEvent.SystemEndpoint.class,
-                KafkaEvent.class,
-                KafkaEvent.KafkaEventRecord.class,
-                KafkaEvent.TopicPartition.class,
-                ScheduledEvent.class,
                 SNSEvent.class,
                 SNSEvent.SNS.class,
                 SNSEvent.SNSRecord.class,
                 SNSEvent.MessageAttribute.class,
+
                 SQSEvent.class,
                 SQSEvent.SQSMessage.class,
                 SQSEvent.MessageAttribute.class,
@@ -63,19 +48,6 @@ import io.goodforgod.graalvm.hint.annotation.TypeHint;
                 S3BatchEvent.class,
                 S3BatchEvent.Job.class,
                 S3BatchEvent.Task.class,
-                S3EventNotification.class,
-                S3EventNotification.S3Entity.class,
-                S3EventNotification.S3BucketEntity.class,
-                S3EventNotification.S3ObjectEntity.class,
-                S3EventNotification.S3EventNotificationRecord.class,
-                S3EventNotification.RequestParametersEntity.class,
-                S3EventNotification.ResponseElementsEntity.class,
-                S3EventNotification.UserIdentityEntity.class,
-                S3ObjectLambdaEvent.class,
-                S3ObjectLambdaEvent.UserRequest.class,
-                S3ObjectLambdaEvent.UserIdentity.class,
-                S3ObjectLambdaEvent.Configuration.class,
-                S3ObjectLambdaEvent.GetObjectContext.class,
 
                 KinesisEvent.class,
                 KinesisEvent.Record.class,
@@ -83,13 +55,6 @@ import io.goodforgod.graalvm.hint.annotation.TypeHint;
 
                 DynamoDBEvent.class,
                 DynamoDBEvent.DynamodbStreamRecord.class,
-
-                CognitoEvent.class,
-                CognitoEvent.DatasetRecord.class,
-                CognitoUserPoolEvent.class,
-                CognitoUserPoolEvent.Request.class,
-                CognitoUserPoolEvent.CallerContext.class,
-
         })
 @InitializationHint(typeNames = {
         "io.goodforgod.aws.simplelambda",
