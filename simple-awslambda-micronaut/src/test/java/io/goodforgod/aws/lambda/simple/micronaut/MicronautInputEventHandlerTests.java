6ebe63f7b73e5c985f8076eb2fe4a1e135b01c4a
Anton Kurako <goodforgod.dev@gmail.com>
2022-04-15 01:11:18 +0300

    [0.26.0-SNAPSHOT] Micronaut module tests updated

--- simple-awslambda-micronaut/src/test/java/io/goodforgod/aws/lambda/simple/micronaut/MicronautInputEventHandlerTests.java
+++ simple-awslambda-micronaut/src/test/java/io/goodforgod/aws/lambda/simple/micronaut/MicronautInputEventHandlerTests.java
@@ -1,20 +1,9 @@
 package io.goodforgod.aws.lambda.simple.micronaut;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import io.goodforgod.aws.lambda.simple.EventContextBuilder;
-import io.goodforgod.aws.lambda.simple.handler.EventHandler;
-import io.goodforgod.aws.lambda.simple.handler.impl.InputEventHandler;
-import io.goodforgod.aws.lambda.simple.reactive.SubscriberUtils;
-import io.goodforgod.aws.lambda.simple.runtime.RuntimeContext;
-import io.goodforgod.aws.lambda.simple.utils.InputStreamUtils;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.util.UUID;
-import java.util.concurrent.Flow.Publisher;
-import org.junit.jupiter.api.AfterAll;
+import io.goodforgod.aws.lambda.simple.micronaut.mock.Request;
+import io.goodforgod.aws.lambda.simple.micronaut.mock.Response;
+import io.goodforgod.aws.lambda.simple.testing.AwsLambdaAssertions;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -23,32 +12,13 @@ import org.junit.jupiter.api.Test;
  */
 class MicronautInputEventHandlerTests extends Assertions {
 
-    private static final RuntimeContext CONTEXT = new MicronautBodyLambdaEntrypoint().getRuntimeContext();
-
-    @BeforeAll
-    public static void setup() {
-        CONTEXT.setupInRuntime();
-    }
-
-    @AfterAll
-    public static void cleanup() throws Exception {
-        CONTEXT.close();
-    }
-
     @Test
-    void inputEventHandled() {
-        final RequestHandler requestHandler = CONTEXT.getBean(RequestHandler.class);
-        final EventHandler handler = CONTEXT.getBean(InputEventHandler.class);
+    void eventHandled() {
+        final Request request = new Request("Steeven King");
+        final Response response = AwsLambdaAssertions.ofEntrypoint(new MicronautInputLambdaEntrypoint())
+                .inputJson(request)
+                .expectJson(Response.class);
 
-        final String eventAsString = "{\"name\":\"Steeven King\"}";
-        final InputStream inputStream = InputStreamUtils.getInputStreamFromStringUTF8(eventAsString);
-
-        final Context eventContext = EventContextBuilder.builder().setAwsRequestId(UUID.randomUUID().toString()).build();
-        final Publisher<ByteBuffer> publisher = handler.handle(requestHandler, inputStream, eventContext);
-        assertNotNull(publisher);
-
-        final String responseAsString = SubscriberUtils.getPublisherString(publisher);
-        assertNotNull(responseAsString);
-        assertTrue(responseAsString.contains("Hello - Steeven King"));
+        assertEquals("Hello - Steeven King", response.message());
     }
 }
