4e35ed1ffafdfe1ef904c3a423698ecd73e18670
Anton Kurako <goodforgod.dev@gmail.com>
2022-05-19 22:59:07 +0300

    [0.28.0-SNAPSHOT] GraalVM native tests preparation

--- simple-awslambda-micronaut/src/test/java/io/goodforgod/aws/lambda/simple/micronaut/mock/Request.java
+++ simple-awslambda-micronaut/src/test/java/io/goodforgod/aws/lambda/simple/micronaut/mock/Request.java
@@ -1,7 +1,10 @@
 package io.goodforgod.aws.lambda.simple.micronaut.mock;
 
+import io.goodforgod.graalvm.hint.annotation.ReflectionHint;
+
 /**
  * @author Anton Kurako (GoodforGod)
  * @since 21.3.2021
  */
+@ReflectionHint
 public record Request(String name) {}
--- simple-awslambda/src/test/java/io/goodforgod/aws/lambda/simple/mock/Response.java
+++ simple-awslambda/src/test/java/io/goodforgod/aws/lambda/simple/mock/Response.java
@@ -1,7 +1,10 @@
 package io.goodforgod.aws.lambda.simple.mock;
 
+import io.goodforgod.graalvm.hint.annotation.ReflectionHint;
+
 /**
  * @author Anton Kurako (GoodforGod)
  * @since 21.3.2021
  */
+@ReflectionHint
 public record Response(String id, String message) {}
