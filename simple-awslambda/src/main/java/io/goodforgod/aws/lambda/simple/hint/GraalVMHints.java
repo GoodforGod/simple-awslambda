f56987a4b56121a31312a3919ada80ac7cbb3112
Anton Kurako <goodforgod.dev@gmail.com>
2022-08-24 23:30:47 +0300

    [0.29.0-SNAPSHOT] GraalVMHints cleanup Dependencies updated

--- simple-awslambda/src/main/java/io/goodforgod/aws/lambda/simple/hint/GraalVMHints.java
+++ simple-awslambda/src/main/java/io/goodforgod/aws/lambda/simple/hint/GraalVMHints.java
@@ -12,55 +12,7 @@ import io.goodforgod.graalvm.hint.annotation.ReflectionHint;
  * @author Anton Kurako (GoodforGod)
  * @since 27.09.2021
  */
-@ReflectionHint(
-        value = { ReflectionHint.AccessType.ALL_DECLARED },
-        types = {
-                APIGatewayProxyEvent.class,
-                APIGatewayProxyEvent.RequestIdentity.class,
-                APIGatewayProxyEvent.ProxyRequestContext.class,
-                APIGatewayProxyResponse.class,
-                APIGatewayV2HTTPEvent.class,
-                APIGatewayV2HTTPEvent.RequestContext.class,
-                APIGatewayV2HTTPEvent.RequestContext.Http.class,
-                APIGatewayV2HTTPEvent.RequestContext.IAM.class,
-                APIGatewayV2HTTPEvent.RequestContext.CognitoIdentity.class,
-                APIGatewayV2HTTPEvent.RequestContext.Authorizer.class,
-                APIGatewayV2HTTPEvent.RequestContext.Authorizer.JWT.class,
-                APIGatewayV2HTTPResponse.class,
-                APIGatewayV2WebSocketEvent.class,
-                APIGatewayV2WebSocketEvent.RequestContext.class,
-                APIGatewayV2WebSocketEvent.RequestIdentity.class,
-                APIGatewayV2WebSocketResponse.class,
-
-                BodyEvent.class,
-                Base64BodyEvent.class,
-
-                SNSEvent.class,
-                SNSEvent.SNS.class,
-                SNSEvent.SNSRecord.class,
-                SNSEvent.MessageAttribute.class,
-
-                SQSEvent.class,
-                SQSEvent.SQSMessage.class,
-                SQSEvent.MessageAttribute.class,
-
-                S3BatchEvent.class,
-                S3BatchEvent.Job.class,
-                S3BatchEvent.Task.class,
-
-                KinesisEvent.class,
-                KinesisEvent.Record.class,
-                KinesisEvent.KinesisEventRecord.class,
-
-                DynamoDBEvent.class,
-                DynamoDBEvent.DynamoDBStreamRecord.class,
-        })
-@InitializationHint(
-        typeNames = {
-                "io.goodforgod.aws.lambda.simple",
-                "io.goodforgod.http.common",
-        },
-        value = InitializationHint.InitPhase.BUILD)
+@InitializationHint( typeNames = { "io.goodforgod.aws.lambda.simple", }, value = InitializationHint.InitPhase.BUILD)
 final class GraalVMHints {
 
     private GraalVMHints() {}
